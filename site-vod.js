58021c213b81eb8c602ff86be74bd2a808e33176
Pierre-Alain8 <pierrealainagn@gmail.com>
2019-11-12 12:55:37 +0100

    cover-flow

---
+++ site-vod.js
@@ -0,0 +1,10 @@
+// coverFlow
+$(function() {
+	$('.coverflow').coverflow();
+});
+// $(function() {
+// 	$( ".cover" ).click(function() {
+//     	$( ".active" ).removeClass( "active" );
+//     	$( this ).addClass( "active" );
+// 	});
+// });
