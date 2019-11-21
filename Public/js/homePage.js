41e1811eaf9343bcce13b56f4f04b5ef8cd4bccb
Pierre-Alain8 <pierrealainagn@gmail.com>
2019-11-21 22:04:25 +0100

    réglage coverflow

--- Public/js/homePage.js
+++ Public/js/homePage.js
@@ -53,12 +53,12 @@ $('#dot1, #dot2, #dot3, #dot4').click(function(n){
 
 // cover flow
 
-// $(function() {
-// 	$( ".cover" ).click(function() {
-//     	$( ".active" ).removeClass( "active" );
-//     	$( this ).addClass( "active" );
-// 	});
-// });
+$(function() {
+	$( ".cover" ).click(function() {
+    	$( ".active" ).removeClass( "active" );
+    	$( this ).addClass( "active" );
+	});
+});
 
 
 });
