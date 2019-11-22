ad0de3764c9ea40ab9274e74c1550133e9e454a6
Pierre-Alain8 <pierrealainagn@gmail.com>
2019-11-22 14:01:20 +0100

    début de la structure html de la page des abonnés qui sera généré dynamiquement via js

--- Public/js/listingPage.js
+++ Public/js/listingPage.js
@@ -1,21 +1,5 @@
 $(document).ready(function() {
-	$('#connexion').click(function(){
-		$('.modal').show();
-		$('.modal-bg').show();
-	});
-	$('.modal .close').click(function(){
-		$('.modal').hide();
-		$('.modal-bg').hide();
-	})
 
-	$("#home, #logo").click(function(){
-	$(location).attr('href',"homePage.html");
-})
-	$(".subscribe").click(function(){
-	$(location).attr('href',"subscribePage.html");
-})
-
-  
 /* Slideshow JavaScript */
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -60,6 +44,5 @@ $(function() {
 	});
 	
 });
-
 
 });
